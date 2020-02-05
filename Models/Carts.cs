89f7d1918640e912856a42b02a4057a3805710e1
Rizquha Maulida <Rizquha.Maulida@bsi.co.id>
2020-02-05 14:08:01 +0700

    first

---
+++ Models/Carts.cs
@@ -0,0 +1,10 @@
+namespace Task_Web_Product.Models
+{
+    public class Carts
+    {
+        public int id {get;set;}
+        public int total {get;set;}
+        public int price {get;set;}
+        
+    }
+}
---
+++ Models/Items.cs
@@ -0,0 +1,12 @@
+namespace Task_Web_Product.Models
+{
+    public class Items
+    {
+        public int id {get;set;}
+        public string title {get;set;}
+        public int rate {get;set;}
+        public int price {get;set;}
+        public string desc {get;set;}
+        public string image {get;set;}
+    }
+}
